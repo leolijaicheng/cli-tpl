d9108b5a0364b3f91d5afc847243790cd3649230
leoli <123@gmail.com>
2020-12-10 20:04:06 +0800

    增加compression-webpack-plugin

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,7 +1,7 @@
 const merge = require('webpack-merge').default
 const base = require('./webpack.base')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-// const CompressionPlugin = require('compression-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 
 module.exports = merge(base,{
@@ -9,10 +9,10 @@ module.exports = merge(base,{
     plugins:[
         new CleanWebpackPlugin(),
 
-    //    new CompressionPlugin({
-    //        test:/\.(js|html|css)$/,
-    //        threshold:10240,
-    //        deleteOriginalAssets:false
-    //    })
+       new CompressionPlugin({
+           test:/\.(js|html|css)$/,
+           threshold:10240,
+           deleteOriginalAssets:false
+       })
     ]
 })
